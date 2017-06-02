58597355bb68849a74a1dc765e14548065ca05e1
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2017-06-02 15:25:18 -0500

    2.2

--- scraper.py
+++ scraper.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
+import threading
+import time
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import lxml
-import threading
-import json
 import schedule
-import time
 
 class wikipediaPage:
     name = ''
@@ -18,49 +18,49 @@ class wikipediaPage:
 data = {}
 data['nodes'] = []
 data['links'] = []
-
-maxNodes = 300
 currentNodes = 0
+maxNodes = 100
+termine = False
 
-initialUrl = "http://en.wikipedia.org/wiki/GitHub"
+initialUrl = "https://en.wikipedia.org/wiki/Music"
 
 def scrape(aUrl):
-    print("page to scrape: " , aUrl)
+    global currentNodes
+    currentNodes = currentNodes + 1
     page = requests.get(aUrl).text
     soup = BeautifulSoup(page, 'lxml')
     newPage = wikipediaPage()
     newPage.link = aUrl
     newPage.name = soup.find('h1').text
-
-    pageLinks = soup.find('div', id="mw-content-text").find_all('a')
-    pageHrefs = list(set(list( map( getLink , pageLinks ) )))
-    filteredPageHrefs = list(set(list(filter( lambda s: s != None , pageHrefs))))
+    pageLinks = soup.find('div',id="mw-content-text").find_all('a', limit=40)
+    pageHrefs = list(set(list( map( getLink, pageLinks))))
+    filteredPageHrefs = list(set(list(filter( lambda s: s!=None ,pageHrefs))))
     newPage.nodes = filteredPageHrefs
-
-    global maxNodes, currentNodes
-    if currentNodes < maxNodes:
-        # toAddEdges = list( map( lambda x: addEdge(newPage.link , x ) , newPage.nodes ) )
-        toAddNodes = list( map( lambda x: addNode(x, newPage.link) , newPage.nodes ) )
-        currentNodes = currentNodes + 1
-        print(currentNodes, "of" , maxNodes)
+    global termine
+    if not termine:
+        toAddNodes = list( map( lambda x: addNode(x,newPage.link), newPage.nodes ))
         explore = list(map(lambda x: addThread(x), newPage.nodes))
+    elif threading.activeCount() < 3:
+        writeJSON()
 
 def addThread(urlToScrape):
-    global maxNodes,currentNodes
-    if currentNodes<maxNodes:
-        currentNodes = currentNodes + 1
-        print(currentNodes , maxNodes)
-        print(currentNodes<maxNodes)
-        ts  = threading.Thread( target = scrape , args=[urlToScrape] )
-        ts.start()
+    global maxNodes
+    if currentNodes < maxNodes:
+        if threading.activeCount()<20000:
+            print( currentNodes, maxNodes, "page to scrape: " , urlToScrape)
+            ts = threading.Thread(target=scrape, args=[urlToScrape])
+            ts.start()
+    else:
+        global termine
+        termine = True
+
+        
 
 def addInicio():
     data['nodes'].append({
         'id': "INICIO",
-        'group': 10
+        'group': 1
     })
-    writeJSON()
-
 
 def addNode(newNodeId, father):
     data['nodes'].append({
@@ -70,12 +70,12 @@ def addNode(newNodeId, father):
     data['links'].append({
         'source': father,
         'target': newNodeId,
-        'value': 20
+        'value': 1
     })
 
 def getLink(a):
-    if a.get("href")!=None and a.get("href").startswith("/wiki/") and not(a.get("href").endswith(".jpg" or ".svg" or ".jpeg")):
-        return str("http://wikipedia.org" + a.get("href") )
+    if a.get("href") != None and a.get("href").startswith("/wiki/") and not a.get("href").endswith(".jpg" or ".svg" or ".jpeg"):
+        return str("http://wikipedia.org" + a.get("href"))
 
 def writeJSON():
     with open('file.json', 'w') as outfile:
@@ -83,16 +83,13 @@ def writeJSON():
     outfile.close()
 
 def log():
-    print("number of active threads: " , threading.activeCount())
-    print("time: " ,  time.clock())
-    if threading.activeCount() < 20  :
-        print("VOY A ESCRIBIR")
-        writeJSON()
-
+    print("number of active threads: ", threading.activeCount())
+    print("time: ", time.clock())
+    print(termine)
 
 
 addInicio()
-addNode(initialUrl , "INICIO")
+addNode(initialUrl, "INICIO")
 scrape(initialUrl)
 
 
@@ -101,5 +98,5 @@ schedule.every(1).seconds.do(log)
 while True:
     schedule.run_pending()
 
-tsc = threading.Thread(target = scrape , args=[initialUrl] )
+tsc = threading.Thread(target=scrape, args=[initialUrl])
 tsc.start()
